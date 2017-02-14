ac9fc08d59f8dc0131d99eb789b551410245ee58
Unknown <pecard94@gmail.com>
2017-02-14 19:43:55 +0100

    Maj acceuil
    Maj acceuil

--- index.php
+++ index.php
@@ -2,30 +2,24 @@
 <html>
 <head>
 	<title>Find Your Team</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="asset/css/styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="asset/css/styles.css">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="row">
-  		<div class="col-sm-2">
-  			<img src="asset/images/lol_acceuil.png">
-  		</div>
-  		<div class="col-sm-2">
-  			<img src="asset/images/hearthstone_acceuil.png">
-  		</div>
-  		<div class="col-sm-2">
-  			<img src="asset/images/ow_acceuil.png">
-  		</div>
-  		<div class="col-sm-2">
-  			<img src="asset/images/cs_acceuil.png">
-  		</div>
-  		<div class="col-sm-2">
-  			<img src="asset/images/cod_acceuil.png">
-  		</div>
-  		<div class="col-sm-2">
-  			<img src="asset/images/fifa_acceuil.png">
-  		</div>
+
+  <?php include "view/menu.php" ?>
+
+	<div class="row" style="padding-top: 100px; padding-left: 30px;">
+  <?php include "view/carroussel.php" ?>
 	</div>
 </body>
 </html>
---
+++ view/carroussel.php
@@ -0,0 +1,48 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+          <div class="col-sm-2">
+          <img src="asset/images/lol_acceuil.png">
+        </div>
+        <div class="col-sm-2">
+          <img src="asset/images/hearthstone_acceuil.png">
+        </div>
+        <div class="col-sm-2">
+          <img src="asset/images/ow_acceuil.png">
+        </div>
+        <div class="col-sm-2">
+          <img src="asset/images/cs_acceuil.png">
+        </div>
+        <div class="col-sm-2">
+          <img src="asset/images/cod_acceuil.png">
+        </div>
+        <div class="col-sm-2">
+          <img src="asset/images/fifa_acceuil.png">
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="asset/images/lol_acceuil.png">
+      </div>
+
+      <div class="item">
+        <img src="img_flower.jpg" alt="Flower">
+      </div>
+
+      <div class="item">
+        <img src="img_flower2.jpg" alt="Flower">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
