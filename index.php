7e4776d05e54ff74e7cf8918eb0df3733756fc91
Unknown <pecard94@gmail.com>
2017-02-14 20:45:36 +0100

    Carroussel OK
    Carroussel OK

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   <link rel="stylesheet" type="text/css" href="asset/css/styles.css">
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 </head>
 <body>
 
--- view/carroussel.php
+++ view/carroussel.php
@@ -1,48 +1,25 @@
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-          <div class="col-sm-2">
-          <img src="asset/images/lol_acceuil.png">
-        </div>
-        <div class="col-sm-2">
-          <img src="asset/images/hearthstone_acceuil.png">
-        </div>
-        <div class="col-sm-2">
-          <img src="asset/images/ow_acceuil.png">
-        </div>
-        <div class="col-sm-2">
-          <img src="asset/images/cs_acceuil.png">
-        </div>
-        <div class="col-sm-2">
-          <img src="asset/images/cod_acceuil.png">
-        </div>
-        <div class="col-sm-2">
-          <img src="asset/images/fifa_acceuil.png">
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="asset/images/lol_acceuil.png">
-      </div>
-
-      <div class="item">
-        <img src="img_flower.jpg" alt="Flower">
-      </div>
-
-      <div class="item">
-        <img src="img_flower2.jpg" alt="Flower">
-      </div>
+<div class="slideshow">
+  <ul>
+    <div class="col-sm-2">
+      <li><img src="asset/images/lol_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/hearthstone_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/ow_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/cs_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/cod_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/fifa_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/ow_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/cod_acceuil.png" alt="" width="230" height="700" /></li>
+      <li><img src="asset/images/hearthstone_acceuil.png" alt="" width="230" height="700" /></li>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+  </ul>
 </div>
+ 
+<script type="text/javascript">
+   $(function(){
+      setInterval(function(){
+         $(".slideshow ul").animate({marginLeft:-230},800,function(){
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 3500);
+   });
+</script>
