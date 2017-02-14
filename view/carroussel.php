e6007d5477df539ac8569feedd801a1b3396feb4
Unknown <pecard94@gmail.com>
2017-02-14 21:01:08 +0100

    maj
    maj

--- view/carroussel.php
+++ view/carroussel.php
@@ -15,7 +15,7 @@
 <script type="text/javascript">
    $(function(){
       setInterval(function(){
-         $(".slideshow ul").animate({marginLeft:-230},800,function(){
+         $(".slideshow ul").animate({marginLeft:-240},800,function(){
             $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
          })
       }, 3500);
