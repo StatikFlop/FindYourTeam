15d5426ee6e79099a1a1301fe4a0c912cf77b15f
pierreecard <pierre.ecard@epsi.fr>
2017-02-14 16:30:06 +0100

    Ajout image acceuil
    Ajout image acceuil

--- index.php
+++ index.php
@@ -4,8 +4,25 @@
 	<title>Find Your Team</title>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="asset/css/styles.css">
 </head>
 <body>
-
+	<div class="row">
+  		<div class="col">
+  			<img src="asset/images/lol_acceuil.png">
+  		</div>
+  		<div class="col">
+  			<img src="asset/images/wow_acceuil.png">
+  		</div>
+  		<div class="col">
+  			<img src="asset/images/ow_acceuil.png">
+  		</div>
+  		<div class="col">
+  			<img src="asset/images/cs_acceuil.png">
+  		</div>
+  		<div class="col">col</div>
+  		<div class="col">col</div>
+  		<div class="col">col</div>
+	</div>
 </body>
 </html>
