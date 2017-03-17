b75a09f92f77cc6d2d4b418cf36de75f57317c68
pierreecard <pierre.ecard@epsi.fr>
2017-03-17 17:05:10 +0100

    maj
    maj

--- profil.php
+++ profil.php
@@ -8,28 +8,26 @@
 	<body class="my_background">
 		<?php include "view/menu.php" ?>
 		<div class="container">
-			<div class="row">
-				<div class="col-md-offset-2 col-md-8">
-					<div class="container-fluid bg-1 text-center">
-					<br>
-						<img src="http://fakeimg.pl/250x250/" class="img-circle" style="width: 250px; height: 250px; background: black;">
-						<br><br>
-						<label>PSEUDO</label>
-						<br><br>
-					</div>
-				</div>
-			</div>
 
 			<div class="row">
 				<div class="col-sm-3">
 				</div>
-				<div class="col-sm-6" style="background-color: rgba(220, 219, 208, 1)">
+				<div class="col-sm-6">
+					<div class="container-fluid bg-1 text-center">
+						<br>
+							<img src="http://fakeimg.pl/250x250/" class="img-circle" style="width: 250px; height: 250px; background: black;">
+							<br><br>
+							<label>PSEUDO</label>
+							<br><br>
+					</div>
+
 					<br>
+
 					<div class="col-sm-7">
-					test
+						test
 					</div>
 					<div class="col-sm-4">
-						<label style="font: bold 14px/18px Helvetica, Arial, sans-serif;"><img src="asset/images/twitter/Twitter_Social_Icon_Blue.png" style="width: 30px;">@StatikFlop</label>
+						<label style="font: bold 14px/18px Helvetica, Arial, sans-serif;"><img src="asset/images/twitter/Twitter_Social_Icon_Blue.png" style="width: 29px;">@StatikFlop</label>
 					</div>
 
 					<br><br>
@@ -37,7 +35,7 @@
 					test
 					</div>
 					<div class="col-sm-4">
-						<label><img src="asset/images/youtube/YouTube-icon-full_color.png" style="width: 25px;">		@StatikFlop</label>
+						<label><img src="asset/images/youtube/YouTube-icon-full_color.png" style="width: 29px;">		@StatikFlop</label>
 					</div>
 
 					<br><br>
@@ -45,7 +43,15 @@
 					test
 					</div>
 					<div class="col-sm-4">
-						<label><img src="asset/images/twitch/Glitch_Purple_RGB.png" style="width: 25px;">		@StatikFlop</label>
+						<label><img src="asset/images/twitch/Glitch_Purple_RGB.png" style="width: 29px;">		@StatikFlop</label>
+					</div>
+
+					<br><br>
+					<div class="col-sm-7">
+					test
+					</div>
+					<div class="col-sm-4">
+						<label><img src="asset/images/facebook/FB-f-Logo__blue_29.png">		@StatikFlop</label>
 					</div>
 				</div>
 
@@ -61,6 +67,20 @@
 				        </p>
 				        <p class="blog-post-bottom pull-right">
 				          <span class="badge quote-badge">896</span>  ❤
+				        </p>
+				      </div>
+				    </blockquote>
+				    <blockquote class="quote-box">
+				      <p class="quote-text">
+				        Encore un test 
+				      </p>
+				      <hr>
+				      <div class="blog-post-actions">
+				        <p class="blog-post-bottom pull-left">
+				          StatokFlop
+				        </p>
+				        <p class="blog-post-bottom pull-right">
+				          <span class="badge quote-badge">212</span>  ❤
 				        </p>
 				      </div>
 				    </blockquote>
